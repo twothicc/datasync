385879cfe2c315c8a1e5a29c5cbe39f0e0aa0bc7
twothicc <chenenliuu@gmail.com>
2022-08-15 18:00:50 +0800

    Change to update logger

--- app/main.go
+++ app/main.go
@@ -6,14 +6,18 @@ import (
 	"github.com/twothicc/common-go/grpcserver"
 	"github.com/twothicc/common-go/logger"
 	"github.com/twothicc/datasync/tools/env"
+	"go.uber.org/zap/zapcore"
 )
 
 var ctx = context.Background()
 
 func main() {
-	logger.InitLogger(env.IsTest())
+	logger.InitLogger(zapcore.ErrorLevel)
 
 	env.Init(ctx)
+	if env.IsTest() {
+		logger.InitLogger(zapcore.DebugLevel)
+	}
 
 	dependencies := initDependencies()
 
